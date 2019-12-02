4a1c9a4aa5c3fb5a2415de86487b413f16b9dec7
Chandru <chandru89new@gmail.com>
2019-12-02 11:03:31 +0530

    adds a comment for the eventbus

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from "./App.vue";
 import Vue3 from "@vue/composition-api";
 Vue.use(Vue3);
 
+/**
+ * event bus is initiated as a prototype so that any component/page can simpley use it as "this.$eventBus" (in Vue 2.x) or "context.root.$eventBus" in (Vue 3.x)
+ */
 Vue.prototype.$eventBus = new Vue();
 
-/** if we want to use all of the algo-components without having to import them in each page of this app,
-uncomment the next two LoC */
-
+/**
+ * if we want to use all of the algo-components without having to import them in each page of this app, uncomment the next two LoC
+ */
 // import AlgoComps from "../../algo-components";
 // Vue.use(AlgoComps);
 
